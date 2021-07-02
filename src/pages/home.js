1a5d0d40f2ff23b7db6f88980e575b1396d07d11
Augusto Otto Molke <augustomolke@gmail.com>
2021-07-02 16:11:33 -0300

    fix columns para small devices

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,7 +13,7 @@ const Home = () => {
         bsPrefix="jumbotron bg-neongreen homecontent"
       >
         <Row>
-          <Col className="center">
+          <Col sm={1} className="center">
             <h1>Uma frase de efeito!</h1>
             <p>
               Alguma frase legal para atrair leads
