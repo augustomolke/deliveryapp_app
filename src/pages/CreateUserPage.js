34d50a9a45984ab671cafcc386b92816840f3531
Augusto Otto Molke <augustomolke@gmail.com>
2021-07-03 12:48:37 -0300

    responsiveness

--- src/pages/CreateUserPage.js
+++ src/pages/CreateUserPage.js
@@ -9,16 +9,25 @@ export default function CreateUserPage() {
     <>
       <Jumbotron className="center" bsPrefix="jumbotron fullpage bg-rosequartz">
         <Col bsPrefix="col containetOverlap">
-          <Row sm="2" bsPrefix="row toprow">
-            <Col bsPrefix="col center">
+          <Row bsPrefix="toprow row">
+            <Col bsPrefix="center col">
               <CreateUserForm />
             </Col>
-            <Col bsPrefix="col center">
+            <Col bsPrefix="center col">
               <h1>Uma frase de efeito!</h1>
               <p>
                 Alguma frase legal para atrair leads
                 <strong> através de uma informação relevante </strong>e um botão
-                para criar conta.
+                para criar conta.Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Suspendisse non convallis nisi, quis
+                sollicitudin odio. Vivamus sodales convallis imperdiet. Aenean
+                dictum tempor eros, ac scelerisque mauris. Proin iaculis augue
+                eu sollicitudin interdum. In in magna sed leo tincidunt luctus.
+                Proin vel nisl maximus, auctor est quis, sollicitudin magna.
+                Nunc pretium lacus imperdiet, faucibus eros eget, consectetur
+                mauris. Morbi nec pretium urna. Etiam scelerisque laoreet
+                egestas. Nullam pharetra efficitur commodo. Pellentesque eget
+                justo cursus, gravida tortor sit amet.
               </p>
             </Col>
           </Row>
