3993e7f6a791f9c73b1e4ef8d34e81340b49d854
Augusto Otto Molke <augustomolke@gmail.com>
2021-07-07 11:38:50 -0300

    add restaurant search

--- src/components/ResourceListing.js
+++ src/components/ResourceListing.js
@@ -3,8 +3,24 @@ import ResourceCard from "./ResourceCard";
 import restimg from "../assets/restimg.jpg";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import restaurantServices from "../services/restaurantServices";
+import { useState } from "react";
 
 const ResourceListing = () => {
+  const [list, setList] = useState([]);
+
+  const getRests = (name) => {
+    restaurantServices.getRestaurants(name, 0, 30).then(
+      (res) => {
+        console.log(res.data);
+        setList(res.data);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  };
+
   return (
     <Jumbotron bsPrefix="jumbotron listingContainer center bg-lighterrose">
       <InputGroup className="mb-3" size="lg">
@@ -13,49 +29,22 @@ const ResourceListing = () => {
           placeholder="What's your craving for today?"
           aria-label="Search"
           aria-describedby="basic-addon1"
+          onChange={(e) => getRests(e.target.value)}
         />
       </InputGroup>
       <Jumbotron bsPrefix="bg-lighterrose listing jumbotron">
-        <ResourceCard
-          name="Nome do restaurante/prato"
-          img={restimg}
-          description="algum texto descritivo sobre  prato ourestaurante"
-          rating="4.8"
-          price="29,99"
-          freight="8.99"
-        />
-        <ResourceCard
-          name="Nome do restaurante/prato"
-          img={restimg}
-          description="algum texto descritivo sobre  prato ourestaurante"
-          rating="4.8"
-          price="29,99"
-          freight="8.99"
-        />
-        <ResourceCard
-          name="Nome do restaurante/prato"
-          img={restimg}
-          description="algum texto descritivo sobre  prato ourestaurante"
-          rating="4.8"
-          price="29,99"
-          freight="8.99"
-        />
-        <ResourceCard
-          name="Nome do restaurante/prato"
-          img={restimg}
-          description="algum texto descritivo sobre  prato ourestaurante"
-          rating="4.8"
-          price="29,99"
-          freight="8.99"
-        />
-        <ResourceCard
-          name="Nome do restaurante/prato"
-          img={restimg}
-          description="algum texto descritivo sobre  prato ourestaurante"
-          rating="4.8"
-          price="29,99"
-          freight="8.99"
-        />
+        {list.map((i) => {
+          return (
+            <ResourceCard
+              key={i._id}
+              name={i.name}
+              img={restimg}
+              description="algum texto descritivo sobre  prato ourestaurante"
+              rating={i.rating}
+              freight={i.freight}
+            />
+          );
+        })}
       </Jumbotron>
     </Jumbotron>
   );
